f408092921799fed2ef8a74f158aa2c01aa46e33
BerkinAkkaya <berkin_akkaya@hotmail.com>
2021-04-03 14:23:40 +0000

    Update Service Worker

--- docs/sw.js
+++ docs/sw.js
@@ -4,57 +4,46 @@ var APP_PREFIX = 'bChat'
 var VERSION = 'version_01'
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
-  '/bChat/',
-  '/bChat/index.html'
+	'/bChat/',
+	'/bChat/index.html',
+	'https://www.gstatic.com/firebasejs/8.2.4/firebase-app.js',
+	'https://www.gstatic.com/firebasejs/8.2.4/firebase-analytics.js',
+	'https://www.gstatic.com/firebasejs/8.2.4/firebase-firestore.js',
+	'https://www.gstatic.com/firebasejs/8.2.4/firebase-auth.js'
 ]
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
+	e.respondWith(
+		caches.match(e.request).then(function (request) {
+			return request || fetch(e.request)
+		})
+	)
 })
 
 // Cache resources
 self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
+	e.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			return cache.addAll(URLS)
+		})
+	)
 })
 
 // Delete outdated caches
 self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
+	e.waitUntil(
+		caches.keys().then(function (keyList) {
+			const cacheWhitelist = keyList.filter(function (key) {
+				return key.indexOf(APP_PREFIX)
+			})
+			cacheWhitelist.push(CACHE_NAME)
 
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
+			return Promise.all(keyList.map((key, i) => {
+				if (cacheWhitelist.indexOf(key) === -1) {
+					return caches.delete(keyList[i])
+				}
+			}))
+		})
+	)
 })
